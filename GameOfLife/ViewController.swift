76452e5b42e234721ee0e3df51059029bb2056e4
Diego Llamas <diegollams@gmail.com>
2015-11-18 08:07:56 -0600

    Initial Commit

---
+++ GameOfLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Diego Llamas Velasco on 11/18/15.
+//  Copyright © 2015 Diego Llamas Velasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
